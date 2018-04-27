3e7ab2bf31ac308b781c111e0e0bf061aaa94dca
Antonin Prouteau <33963104+phycelot@users.noreply.github.com>
2018-04-27 10:24:06 +0200

    Delete dbconnection.php

--- dbconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host_name  = "db679042178.db.1and1.com";
-$database   = "db679042178";
-$user_name  = "dbo679042178";
-$password   = "barometre";
-$connect = mysqli_connect($host_name, $user_name, $password, $database);
-?>
